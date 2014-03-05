89d185c8aaa457f7f0553b9682b046c9a1ccadca
Leo <leoreyes1001@gmail.com>
2014-03-05 15:05:37 -0600

    Updated crawler
    Searches url for contacts page and searches contacts page for email

--- AutoSearch.py
+++ AutoSearch.py
@@ -5,6 +5,7 @@ Created on Nov 17, 2013
 @author: leoreyes
 '''
 from google import search
+from Crawler import findContactPage
 
 if __name__ == '__main__':
     inputFileName = raw_input("Enter name of input file: ")
@@ -12,13 +13,27 @@ if __name__ == '__main__':
     writeFile = open(writeFileName, "a")
     with open(inputFileName) as file:
         for line in file:
-          searchKeyWord = line
-          numTemp=1
-          stopTemp=1
-          url = search(searchKeyWord,num=numTemp,stop=stopTemp,pause=2.0)
-          writeFile.write(url)
-          writeFile.write("\n")
-          print(url)
+            searchKeyWord = line
+            numTemp=1
+            stopTemp=1
+            url = search(searchKeyWord,num=numTemp,stop=stopTemp,pause=5.0)
+            writeFile.write(url)
+            #writeFile.write("\n")
+            contactStr = findContactPage(url)
+            print contactStr
+        if(len(contactStr) > 0):
+            contactPage = google.get_page(contactStr[0].get("href"))
+            print contactStr[0].get("href")#.find_parents("a")
+            soup = BeautifulSoup(contactPage)
+            emailStr = soup.find_all(text=re.compile("[\w\.-]+@[\w\.-]+"))
+            if(len(emailStr) > 0) :
+                writeFile.write("\t" + emailStr + "\n")
+                print emailStr
+            else:
+                print "could not find email"
+        else:
+            writeFile.write("\n")
+            print "could not find contacts page"
     file.close()
     
     pass
--- Crawler.py
+++ Crawler.py
@@ -1,8 +1,26 @@
+import google
+import re
+from bs4 import BeautifulSoup
 
-# templates for Google search
 
-urlGoogleHome = "http://www.google.%(tld)s/"
-urlGoogleSearch = "http://www.google.$(tld)s/search?hl=%(lang)s&q=%(query)s&btnG=Google+Search"
-urlGoogleNextPage     = "http://www.google.%(tld)s/search?hl=%(lang)s&q=%(query)s&start=%(start)d"
-urlGoogleSearchNum    = "http://www.google.%(tld)s/search?hl=%(lang)s&q=%(query)s&num=%(num)d&btnG=Google+Search"
-urlGoogleNextPageNum = "http://www.google.%(tld)s/search?hl=%(lang)s&q=%(query)s&num=%(num)d&start=%(start)d"
+def findContactPage(url):
+    html = google.get_page(url)
+    soup = BeautifulSoup(html)
+    contactStr = soup.find_all('a', href=re.compile(".*?contact", re.IGNORECASE))
+    return contactStr
+
+
+if __name__ == "__main__":
+    url = "http://www.wrangler.com/"
+    contactStr = findContactPage(url)
+    if(len(contactStr) > 0):
+        contactPage = google.get_page(contactStr[0].get("href"))
+        print contactStr[0].get("href")#.find_parents("a")
+        soup = BeautifulSoup(contactPage)
+        emailStr = soup.find_all(text=re.compile("[\w\.-]+@[\w\.-]+"))
+        if(len(emailStr) > 0) :
+            print addressStr
+        else:
+            print "could not find email"
+    else:
+        print "could not find contacts page"
